261b4dc531b38f05dfab5d6732de18f83ba7023c
Najman Husaini <najmanhusaini20@gmail.com>
2024-09-12 12:40:19 -0600

    first commit

---
+++ watchmecode-client/src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import CodeMirror from "svelte-codemirror-editor";
+    import { defaultKeymap } from "@codemirror/commands";
+    import { keymap } from "@codemirror/view";
+	import { onDestroy } from "svelte";
+	import { browser } from "$app/environment";
+
+    function generateRandomString(length: number) {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+    
+    let value = "";
+
+    if (browser) {
+        let lastValue = "";
+        let id = generateRandomString(12);
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const host = urlParams.get('host') ?? "127.0.0.1";
+        console.log(host);
+
+        async function postCode() {
+            if (lastValue == value) {
+                return;
+            }
+            lastValue = value;
+            fetch(`http://${host}/code/${id}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "text/plain",
+                },
+                body: value
+            });
+        }
+
+        const interval = setInterval(postCode, 2000);
+
+        onDestroy(() => {
+            clearInterval(interval);
+        });
+    }
+
+</script>
+
+<CodeMirror bind:value basic={false} extensions={[keymap.of([
+    ...defaultKeymap,
+  ])
+]}/>
---
+++ watchmecode-host/src/main.rs
@@ -0,0 +1,30 @@
+use axum::{extract::Path, routing::post, Router};
+use tokio::fs::{create_dir_all, read_dir, remove_file, write};
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
+
+#[axum::debug_handler]
+async fn post_code(
+    Path((id,)): Path<(String,)>, code: String) {
+        write(&format!("incoming/{id}.txt"), code.as_bytes()).await.unwrap();
+}
+
+
+#[tokio::main]
+async fn main() {
+    create_dir_all("incoming").await.unwrap();
+    let mut read_dir_iter = read_dir("incoming").await.unwrap();
+    
+    while let Some(entry) = read_dir_iter.next_entry().await.unwrap() {
+        tokio::spawn(remove_file(entry.path()));
+    }
+    
+    let app = Router::new().route("/code/:id", post(post_code))
+        .layer(
+                ServiceBuilder::new()
+                    .layer(CorsLayer::new().allow_origin(Any).allow_methods(Any))
+            );
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
