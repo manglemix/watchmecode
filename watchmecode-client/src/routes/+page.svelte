3243cb44a258df825a6d96cb3f9fa34e4ba126ae
Najman Husaini <najmanhusaini20@gmail.com>
2024-09-12 19:57:21 -0600

    revert

--- watchmecode-client/src/routes/+page.svelte
+++ watchmecode-client/src/routes/+page.svelte
@@ -4,13 +4,23 @@
     import { keymap } from "@codemirror/view";
 	import { onDestroy } from "svelte";
 	import { browser } from "$app/environment";
+
+    function generateRandomString(length: number): string {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
     
     let value = "";
     let name = "";
     let lastValue = "";
 
     if (browser) {
-
+        let id = generateRandomString(16);
         const urlParams = new URLSearchParams(window.location.search);
         const host = urlParams.get('host') ?? "http://127.0.0.1";
 
@@ -21,9 +31,9 @@
             lastValue = value;
             let url;
             if (name.length > 0) {
-                url = `${host}/code/${name}/`;
+                url = `${host}/code/${id}/${name}/`;
             } else {
-                url = `${host}/code/`;
+                url = `${host}/code/${id}/`;
             }
             fetch(url, {
                 method: "POST",
@@ -32,7 +42,6 @@
                 },
                 body: value,
                 mode: 'no-cors',
-                credentials: 'include'
             });
         }
 
--- watchmecode-host/src/main.rs
+++ watchmecode-host/src/main.rs
@@ -1,21 +1,40 @@
-#![feature(const_collections_with_hasher, build_hasher_default_const_new, try_blocks)]
-use std::{collections::HashMap, sync::Arc};
+#![feature(
+    const_collections_with_hasher,
+    build_hasher_default_const_new,
+    try_blocks
+)]
 use chrono::{Local, Timelike};
 use parking_lot::RwLock;
+use std::{collections::HashMap, sync::Arc};
 
-use axum::{extract::Path, http::{HeaderValue, Method}, routing::post, Router};
+use axum::{
+    extract::Path,
+    http::{HeaderValue, Method},
+    routing::post,
+    Router,
+};
 use axum_extra::extract::{cookie::Cookie, CookieJar};
 use fxhash::{FxBuildHasher, FxHashMap};
-use rand::{distributions::{Alphanumeric, DistString}, thread_rng};
-use tokio::{fs::{create_dir_all, read_dir, remove_file}, io::{AsyncWriteExt, BufWriter}};
+use rand::{
+    distributions::{Alphanumeric, DistString},
+    thread_rng,
+};
+use tokio::{
+    fs::{create_dir_all, read_dir, remove_file},
+    io::{AsyncWriteExt, BufWriter},
+};
 use tower::ServiceBuilder;
-use tower_http::{compression::CompressionLayer, cors::{AllowOrigin, CorsLayer}};
+use tower_http::{
+    compression::CompressionLayer,
+    cors::{AllowOrigin, CorsLayer},
+};
 
 struct UserInfo {
-    name: Arc<str>
+    name: Arc<str>,
 }
 
-static USERS: RwLock<FxHashMap<String, UserInfo>> = RwLock::new(HashMap::with_hasher(FxBuildHasher::new()));
+static USERS: RwLock<FxHashMap<String, UserInfo>> =
+    RwLock::new(HashMap::with_hasher(FxBuildHasher::new()));
 
 async fn post_code(name: Option<String>, mut jar: CookieJar, code: String) -> CookieJar {
     let id_owned;
@@ -27,7 +46,7 @@ async fn post_code(name: Option<String>, mut jar: CookieJar, code: String) -> Co
             &id_owned
         }
     };
-    
+
     let owned_name: Arc<str>;
     let name = if let Some(name) = name {
         'result: {
@@ -39,7 +58,12 @@ async fn post_code(name: Option<String>, mut jar: CookieJar, code: String) -> Co
             }
             let mut writer = USERS.write();
             owned_name = Arc::from(name.into_boxed_str());
-            writer.insert(id.to_owned(), UserInfo { name: owned_name.clone() });
+            writer.insert(
+                id.to_owned(),
+                UserInfo {
+                    name: owned_name.clone(),
+                },
+            );
             &*owned_name
         }
     } else {
@@ -50,9 +74,13 @@ async fn post_code(name: Option<String>, mut jar: CookieJar, code: String) -> Co
             ""
         }
     };
-    
-    let mut file = BufWriter::new(tokio::fs::File::create(format!("incoming/{id}.txt")).await.expect("Failed to open file"));
-    
+
+    let mut file = BufWriter::new(
+        tokio::fs::File::create(format!("incoming/{id}.txt"))
+            .await
+            .expect("Failed to open file"),
+    );
+
     let result: std::io::Result<()> = try {
         if !name.is_empty() {
             file.write_all(b"Name: ").await?;
@@ -72,12 +100,16 @@ async fn post_code(name: Option<String>, mut jar: CookieJar, code: String) -> Co
     };
     result.expect("Failed to write to file");
     file.flush().await.expect("Failed to flush");
-    
+
     jar
 }
 
 #[axum::debug_handler]
-async fn post_code_with_name(Path((name,)): Path<(String,)>, jar: CookieJar, code: String) -> CookieJar {
+async fn post_code_with_name(
+    Path((name,)): Path<(String,)>,
+    jar: CookieJar,
+    code: String,
+) -> CookieJar {
     post_code(Some(name), jar, code).await
 }
 
@@ -99,23 +131,21 @@ async fn main() {
         .route("/code/:name/", post(post_code_with_name))
         .route("/code/", post(post_code_without_name))
         .layer(
-            ServiceBuilder::new().layer(
-                CorsLayer::new()
-                    .allow_origin(
-                        AllowOrigin::list([
-                            "https://manglemix.github.com".parse::<HeaderValue>().unwrap(),
-                            "http://localhost:5173".parse::<HeaderValue>().unwrap(),
-                            "http://127.0.0.1:5173".parse::<HeaderValue>().unwrap(),
-                        ])
-                    )
-                    .allow_methods([Method::POST])
-                    .allow_credentials(true),
-        )
-        .layer(
-            CompressionLayer::new()
-                .br(true)
-        ),
-    );
+            ServiceBuilder::new()
+                // .layer(
+                //     CorsLayer::new()
+                //         .allow_origin(AllowOrigin::list([
+                //             "https://manglemix.github.com"
+                //                 .parse::<HeaderValue>()
+                //                 .unwrap(),
+                //             "http://localhost:5173".parse::<HeaderValue>().unwrap(),
+                //             "http://127.0.0.1:5173".parse::<HeaderValue>().unwrap(),
+                //         ]))
+                //         .allow_methods([Method::POST])
+                //         .allow_credentials(true),
+                // )
+                .layer(CompressionLayer::new().br(true)),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     axum::serve(listener, app).await.unwrap();
